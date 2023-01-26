9902a379650632f4129c0d70d2a234d009402641
TEkdahl92 <106709941+TEkdahl92@users.noreply.github.com>
2023-01-25 20:52:57 -0600

    added a basic calendar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Calendar from './Calendar';
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>LAS Calendar</h1>
+      <Calendar />
     </div>
   );
 }
