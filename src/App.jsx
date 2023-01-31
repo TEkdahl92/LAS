06a71bd67cc4d5b9a07b1257a43499a561ff1007
TEkdahl92 <106709941+TEkdahl92@users.noreply.github.com>
2023-01-30 21:43:49 -0600

    setup how I want the project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Calendar from "./Components/Calendar";
+import Contact from "./Components/Contact";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+      <Calendar />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
