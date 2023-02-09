13164e0b635dca4a65aa63f90ec0c270eebce3f4
TEkdahl92 <106709941+TEkdahl92@users.noreply.github.com>
2023-02-08 20:48:00 -0600

    created a working basic home page

---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="container">
+      <h1>Welcome to Lydianise's Animal Sitting</h1>
+      <p>This is the home page of my app. Here you will find information about the app and links to other sections.</p>
+      <a href="#" className="btn">Learn More</a>
+    </div>
+  );
+}
+
+export default HomePage;
