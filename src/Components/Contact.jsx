3cb0e7e1c22213d1021f3889caccc7489719ab4a
TEkdahl92 <106709941+TEkdahl92@users.noreply.github.com>
2023-02-08 21:50:02 -0600

    got the contact section made and correctly working except the email. Possible solution, use an API for a contact form

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div class = 'container'>
+      <h2>Contact Information</h2>
+      <p>
+        Email: <a href="mailto:lydianise.saldana@gmail.com">lydianise.saldana@gmail.com</a>
+      </p>
+      <p>
+        Phone: <a href="tel:+1-254-350-1382">(254) 350-1382</a>
+      </p>
+    </div>
+  );
+}
+
+export default Contact;
